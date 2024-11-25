9d50865b78ee2135ad43ee3d6039d2be2731a604
TAASOJOA <miguel.sousaribeiro@swisscom.com>
2024-11-25 08:18:52 +0100

    first navbar idea

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -56,7 +56,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            ZOOHOOO
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
