c21f2f09a4b5352a28f944859b9d20b79b0d09d4
TAASOJOA <miguel.sousaribeiro@swisscom.com>
2024-12-02 10:37:03 +0100

    navbar fix

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -9,81 +9,83 @@ import Tooltip from "@mui/material/Tooltip";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
+import Modal from "./Modal";
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
 function ResponsiveAppBar() {
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
-
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* ZOHOOO Text */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#"
-            sx={{
-              mr: 2,
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            ZOOHOOO
-          </Typography>
-
-          {/* Avatar Icon */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
-                <AccountCircleIcon />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+    <>
+      {" "}
+      <AppBar position="static">
+        {" "}
+        <Container maxWidth="xl">
+          {" "}
+          <Toolbar disableGutters>
+            {" "}
+            {/* ZOHOOO Text */}{" "}
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#"
+              sx={{
+                mr: 2,
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {" "}
+              ZOHOOO{" "}
+            </Typography>{" "}
+            {/* Avatar Icon */}{" "}
+            <Box sx={{ flexGrow: 0 }}>
+              {" "}
+              <Tooltip title="Open settings">
+                {" "}
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  {" "}
+                  <AccountCircleIcon sx={{ width: "50px" }} />{" "}
+                </IconButton>{" "}
+              </Tooltip>{" "}
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                keepMounted
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {" "}
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    {" "}
+                    <Typography sx={{ textAlign: "center" }}>
+                      {" "}
+                      {setting}{" "}
+                    </Typography>{" "}
+                  </MenuItem>
+                ))}{" "}
+              </Menu>{" "}
+            </Box>{" "}
+          </Toolbar>{" "}
+        </Container>{" "}
+      </AppBar>{" "}
+      <Modal></Modal>{" "}
+    </>
   );
 }
-
 export default ResponsiveAppBar;
